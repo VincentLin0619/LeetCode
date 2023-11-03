79b773c98355c984a2704f0f6aaf585906f59b61
Zhe Kuan Lin <53500272+VincentLin0619@users.noreply.github.com>
2023-11-03 14:31:33 +0800

    merge two sroted lists

--- Ruby/21_merge_two_sorted_lists.rb
+++ Ruby/21_merge_two_sorted_lists.rb
@@ -1,14 +1,28 @@
 # Definition for singly-linked list.
-# class ListNode
-#     attr_accessor :val, :next
-#     def initialize(val = 0, _next = nil)
-#         @val = val
-#         @next = _next
-#     end
-# end
+class ListNode
+    attr_accessor :val, :next
+    def initialize(val = 0, _next = nil)
+        @val = val
+        @next = _next
+    end
+end
 # @param {ListNode} list1
 # @param {ListNode} list2
 # @return {ListNode}
 def merge_two_lists(list1, list2)
-
+  list = ListNode.new(0)
+  head = list
+  while list1 && list2
+    if list1.val <= list2.val
+      list.next = list1
+      list1 = list1.next
+    else
+      list.next = list2
+      list2 = list2.next
+    end
+  end
+  list.next = list1 || list2
+  head.next
 end
+
+merge_two_lists([1,2,4], [1,3,4])
